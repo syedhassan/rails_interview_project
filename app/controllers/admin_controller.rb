c6f0228ab1b739cf491cb7f2ccd3130d3dc6c130
Sana Hassan <sana.hassan@orbitz.com>
2015-03-04 23:29:02 -0600

    Wire up the Admin controller path.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,20 @@
 class AdminController < ActionController::Base
 
   def index
+    @listings = Listing.all
+  end
 
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    if @listing.update_attributes(params.require(:listing).permit(:available, :price_per_hour))
+      redirect_to admin_index_path
+    else
+      render 'edit'
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     post 'book', :on => :collection
   end
 
+  resources :admin
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
